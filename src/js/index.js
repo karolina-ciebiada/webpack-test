a5c0917570ccf861957a8e8045ad75556b8a972d
Karolina Ciebiada <karolina.ciebiada@icloud.com>
2020-06-08 14:59:12 +0200

    script added

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,16 @@
 import '../scss/main.scss';
-
+import moment from 'moment';
 /* place your code below */
 
 console.log('HELLO 🚀')
+const name='Karo';
+const age=19;
+
+console.log(
+    `nazywam się ${name} i mam ${age} lat.`)
+const startOfDay= moment().startOf('day').fromNow();
+const timePlaceholder = document.querySelector('.time--js');
+timePlaceholder.innerHTML = startOfDay;
+
+
+console.log(startOfDay)
